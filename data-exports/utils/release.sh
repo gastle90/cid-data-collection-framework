5dd031c1ad172b577174436c96a3938566ed14a9
Iakov Gan <iakov@amazon.com>
2025-01-23 14:24:33 +0100

    fix release script for DE

--- data-exports/utils/release.sh
+++ data-exports/utils/release.sh
@@ -9,7 +9,7 @@ for file in "${files[@]}"; do
     version=$(grep '^Description:' "data-exports/deploy/${file}.yaml" | grep -o '[0-9]\+\.[0-9]\+\.[0-9]\+')
     source_path="data-exports/deploy/${file}.yaml"
     echo $source_path
-    aws s3 sync "$source_path" "s3://$CENTRAL_BUCKET/cfn/data-exports/$version/${file}.yaml"
-    aws s3 sync "$source_path" "s3://$CENTRAL_BUCKET/cfn/data-exports/latest/${file}.yaml"
-    aws s3 sync "$source_path" "s3://$CENTRAL_BUCKET/cfn/${file}.yaml"
+    aws s3 cp "$source_path" "s3://$CENTRAL_BUCKET/cfn/data-exports/$version/${file}.yaml"
+    aws s3 cp "$source_path" "s3://$CENTRAL_BUCKET/cfn/data-exports/latest/${file}.yaml"
+    aws s3 cp "$source_path" "s3://$CENTRAL_BUCKET/cfn/${file}.yaml"
 done
