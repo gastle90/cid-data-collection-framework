b9ddf821be6fa1a8c0645a66463a133a6369f52e
Raffy Armistead <126702050+rarmist@users.noreply.github.com>
2025-03-15 15:15:20 -0700

    All policies were updated with resource-level restrictions for supported actions
    Co-authored-by: Iakov GAN <82834333+iakov-aws@users.noreply.github.com>
    Co-authored-by: Iakov Gan <iakov@amazon.com>

--- test/test_from_scratch.py
+++ test/test_from_scratch.py
@@ -125,9 +125,9 @@ def test_trusted_advisor_data(athena):
     data = athena_query(athena=athena, sql_query='SELECT * FROM "optimization_data"."trusted_advisor_data" LIMIT 10;')
     assert len(data) > 0, 'trusted_advisor_data is empty'
 
-def test_inventory_workspaces_metrics_data(athena):
-    data = athena_query(athena=athena, sql_query='SELECT * FROM "optimization_data"."inventory_workspaces_metrics_data" LIMIT 10;')
-    assert len(data) > 0, 'inventory_workspaces_metrics_data is empty'
+def test_workspaces_metrics_data(athena):
+    data = athena_query(athena=athena, sql_query='SELECT * FROM "optimization_data"."workspaces_metrics_data" LIMIT 10;')
+    assert len(data) > 0, 'workspaces_metrics_data is empty'
 
 def test_transit_gateway_data(athena):
     data = athena_query(athena=athena, sql_query='SELECT * FROM "optimization_data"."transit_gateway_data" LIMIT 10;')
@@ -180,7 +180,7 @@ def test_compute_optimizer_export_triggered(compute_optimizer, start_time):
     jobs = compute_optimizer.describe_recommendation_export_jobs()['recommendationExportJobs']
     logger.debug(f'Jobs in: {jobs}')
     jobs_since_start = [job for job in jobs if job['creationTimestamp'].replace(tzinfo=None) > start_time.replace(tzinfo=None)]
-    assert len(jobs_since_start) == 7, f'started {len(jobs_since_start)} jobs. Expected 7. Not all jobs launched'
+    assert len(jobs_since_start) == 8, f'started {len(jobs_since_start)} jobs. Expected 8. Not all jobs launched'
     jobs_failed = [job for job in jobs_since_start if job.get('status') == 'failed']
     assert len(jobs_failed) == 0, f'Some jobs failed {jobs_failed}'
     # TODO: check how we can add better test, taking into account 15-30 mins delay of export in CO
